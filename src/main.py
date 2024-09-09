3c04fcd1c4329f221fdf8c37324ac491bec1c390
Erik Junsved <dev@erix.dev>
2024-09-09 15:31:43 +0200

    rename types and fix imports

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import sys
 
-from src.utils import load_bodies_file, print_body_arr
+
+
+import utils
 
 if len(sys.argv) != 3:
     print(f"Usage: {sys.argv[0]} [input_file] [output_file]")
     exit(1)
 
 
-bodies = load_bodies_file(sys.argv[1])
+bodies = utils.load_bodies_file(sys.argv[1])
 
-print_body_arr(bodies)
+utils.print_body_arr(bodies)
