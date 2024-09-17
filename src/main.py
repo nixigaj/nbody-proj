ac98de4cf7b55fa9b72f51938d1665718455feb6
Erik Junsved <dev@erix.dev>
2024-09-17 02:32:01 +0200

    fix error message

--- src/main.py
+++ src/main.py
@@ -23,6 +23,15 @@ def calculate(no_iterations, input_file, output_file, full_sim_output_file=None)
     if full_sim_output_file is not None:
         utils.write_particles_arr_file(full_sim_output_file, resulting_particles)
 
+    def update_fun(frame, particles):
+        index = frame % no_iterations
+        updated_particles = resulting_particles[index]
+
+        particles['x_position'][:] = updated_particles['x_position']
+        particles['y_position'][:] = updated_particles['y_position']
+
+    matplot.show_particles_multi(resulting_particles[0], no_iterations, update_fun)
+
 
 def show(no_iterations, full_sim_input_file):
     resulting_particles = utils.load_particles_arr_file(full_sim_input_file, no_iterations)
@@ -45,7 +54,7 @@ if __name__ == "__main__":
     try:
         no_iterations = int(sys.argv[2])
     except ValueError:
-        print(f"Invalid number of iterations: {sys.argv[1]}")
+        print(f"Invalid number of iterations: {sys.argv[2]}")
         usage()
 
     if sys.argv[1] == "calc" and 5 <= len(sys.argv) <= 6:
